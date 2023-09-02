953a8089cd600a2bbd983d1d010c1fe568ecdb81
Krystian Fowler <krystian@stratokit.io>
2023-09-02 17:35:22 +0200

    client:css: reset and base styles

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+</script>
+
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap"
+		rel="stylesheet"
+	/>
+</head>
+
+
+<slot />
